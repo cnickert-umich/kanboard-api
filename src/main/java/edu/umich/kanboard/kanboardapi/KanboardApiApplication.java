150bcce6df50fa4e0beb09b416145f0f19ee448b
Cameron M Nickert <cameron3pointer@gmail.com>
2019-10-08 21:53:16 -0400

    first commit

---
+++ src/main/java/edu/umich/kanboard/kanboardapi/KanboardApiApplication.java
@@ -0,0 +1,13 @@
+package edu.umich.kanboard.kanboardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanboardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanboardApiApplication.class, args);
+	}
+
+}
