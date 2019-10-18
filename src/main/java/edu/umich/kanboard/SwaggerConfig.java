1054341fb03acd66b551cf39e9f54eef2e1ce76c
Cameron M Nickert <cameron3pointer@gmail.com>
2019-10-18 15:33:15 -0400

    Updated the package for swagger docs

--- src/main/java/edu/umich/kanboard/SwaggerConfig.java
+++ src/main/java/edu/umich/kanboard/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package edu.umich.kanboard.kanboardapi;
+package edu.umich.kanboard;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
