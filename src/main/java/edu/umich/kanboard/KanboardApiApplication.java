1fe93829bbe2473d7c1dfef30ca58e2269afc7c8
Cameron M Nickert <cameron3pointer@gmail.com>
2019-10-18 15:32:42 -0400

    Changed the applications package

--- src/main/java/edu/umich/kanboard/KanboardApiApplication.java
+++ src/main/java/edu/umich/kanboard/KanboardApiApplication.java
@@ -1,4 +1,4 @@
-package edu.umich.kanboard.kanboardapi;
+package edu.umich.kanboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
