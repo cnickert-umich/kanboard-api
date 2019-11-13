053528a08a913b9079600b977aaee9762f52b75b
bennerv <10840174+bennerv@users.noreply.github.com>
2019-11-13 00:54:28 -0500

    Create 3 test user stories upon creation of local h2 database

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,3 +1,7 @@
 INSERT INTO column_headers (name) VALUES ('Defined');
 INSERT INTO column_headers (name) VALUES ('In Progress');
 INSERT INTO column_headers (name) VALUES ('Completed');
+
+INSERT INTO user_story (name, description, column_id) VALUES ('Big Test', 'This is a test user story inserted upon creation', 1);
+INSERT INTO user_story (name, description, column_id) VALUES ('Small Test', 'This is a test user story inserted upon creation', 1);
+INSERT INTO user_story (name, description, column_id) VALUES ('Large unit', 'This is a test user story inserted upon creation', 2);
